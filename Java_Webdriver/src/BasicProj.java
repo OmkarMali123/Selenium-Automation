5e44132aba8e3ad57e5761dbf8f43d06ff473c1b
OmkarMali123 <omkarmali19@gmail.com>
2026-03-20 01:37:12 +0530

    First Project commit with Webdriver External Jars Added Process

---
+++ Java_Webdriver/src/BasicProj.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class BasicProj {
+
+	public static void main(String[] args) {
+	//	System.setProperty("webdriver.chrome.driver","path of exe file ");
+	//	WebDriver driver = new ChromeDriver();
+		WebDriver driver= new ChromeDriver();
+		//ChromeDriver dri = new ChromeDriver();
+		driver.get("http:\\google.com");
+		
+	}
+
+}
